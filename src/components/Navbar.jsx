464041f5fb83183cc2befcfc381661e49f3c0181
JhonatanP16 <jhonatanpanduro16@gmail.com>
2023-09-13 19:49:44 -0500

    xd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = ({openNav,onCloseNav}) => {
         flexDirection:'column',
     }}>
         <Toolbar sx={{fontSize:'20px',fontWeight:600,color:'primary.main'}}>
-         I.E. San Sebastian
+        San Sebastian
         </Toolbar>
         <List>
             {
